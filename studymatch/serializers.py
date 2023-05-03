ac437ca452e324d5c0082b3d7d3b053e7abbf1b5
vic m <vm@vics-MacBook-Pro.local>
2023-05-03 16:41:44 +0300

    fix: hash password before saving

--- studymatch/serializers.py
+++ studymatch/serializers.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
+
 from .models import Group, Groupusers
 
 
@@ -13,6 +15,14 @@ class CreateUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["username", "password"]
+
+    def create(self, validated_data):
+        """Hash password before saving user obj."""
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.password = make_password(password)
+        user.save()
+        return user
 
 
 class GroupSerializer(serializers.ModelSerializer):
